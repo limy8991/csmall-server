c2a5e365634bed3da97c233e435158a5d8706757
Lmy <1144846006@qq.co>
2022-06-09 10:29:07 +0800

    Initial commit

---
+++ src/main/java/cn/tedu/csmall/CsmallServerApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallServerApplication.class, args);
+    }
+
+}
