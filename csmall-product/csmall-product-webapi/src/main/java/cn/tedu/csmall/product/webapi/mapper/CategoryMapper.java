45a2ef78965e335ad44ca14b33bd61149326739e
Lmy <1144846006@qq.co>
2022-06-14 11:38:36 +0800

    6.14

--- csmall-product/csmall-product-webapi/src/main/java/cn/tedu/csmall/product/webapi/mapper/CategoryMapper.java
+++ csmall-product/csmall-product-webapi/src/main/java/cn/tedu/csmall/product/webapi/mapper/CategoryMapper.java
@@ -11,13 +11,13 @@ import java.util.List;
 
 /**
  * 处理“类别”数据的持久层接口
- * @author Lmy
  */
 @Repository
 public interface CategoryMapper {
 
     /**
      * 插入“类别”数据
+     *
      * @param category 类别
      * @return 受影响的行数
      */
@@ -25,7 +25,8 @@ public interface CategoryMapper {
 
     /**
      * 根据“类别”id修改“是否为父级类别”
-     * @param id “类别”id
+     *
+     * @param id       “类别”id
      * @param isParent 是否为父级类别，1=是，0=否
      * @return 受影响的行数
      */
@@ -33,6 +34,7 @@ public interface CategoryMapper {
 
     /**
      * 根据“类别”id查询“类别”的简单信息
+     *
      * @param id “类别”id
      * @return “类别”的简单信息
      */
@@ -40,22 +42,26 @@ public interface CategoryMapper {
 
     /**
      * 根据“类别”名称查询“类别”的简单信息
+     *
      * @param name “类别”名称
      * @return “类别”的简单信息
      */
     CategorySimpleVO getByName(String name);
 
     /**
-     * 根据"父级"类别查询所有"子级"类别
-     * @param parentId
-     * @return
+     * 根据“类别”id查询“类别”的详细信息
+     *
+     * @param id “类别”id
+     * @return “类别”的详细信息
+     */
+    CategoryDetailsVO getDetailsById(Long id);
+
+    /**
+     * 根据父级"类别"查询其所有子级"类别"
+     *
+     * @param parentId 父级"类别"的id
+     * @return 此父级"类别"下的所有子级"类别"的列表
      */
     List<CategorySimpleListItemVO> listByParentId(Long parentId);
 
-    /**
-     * 按照id查询
-     * @param id
-     * @return
-     */
-    CategoryDetailsVO getDetailsById(Long id);
 }
