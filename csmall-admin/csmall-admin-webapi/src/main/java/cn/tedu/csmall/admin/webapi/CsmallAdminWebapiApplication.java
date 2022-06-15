d588107d48406d671fac9d0900b1011343fd4b19
Lmy <1144846006@qq.co>
2022-06-15 20:40:53 +0800

    创建管理员

---
+++ csmall-admin/csmall-admin-webapi/src/main/java/cn/tedu/csmall/admin/webapi/CsmallAdminWebapiApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.admin.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallAdminWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallAdminWebapiApplication.class, args);
+    }
+
+}
---
+++ csmall-admin/csmall-admin-webapi/src/test/java/cn/tedu/csmall/admin/webapi/CsmallAdminWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.admin.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallAdminWebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
